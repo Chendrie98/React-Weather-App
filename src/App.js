c6e547dc9f4e6d25c5ef6b5a8e6909406cbf9561
Chendrie98 <98337053+Chendrie98@users.noreply.github.com>
2022-12-31 16:06:51 -0500

    Added a link at the bottom to my Github repository

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import WeatherSearch from "./WeatherSearch";
 import "./App.css";
 import "./index.css";
+import GitHubLink from "./GitHubLink";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <GitHubLink />
     </div>
   );
 }
---
+++ src/GitHubLink.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GitHubLink() {
+  return (
+    <div>
+      <a href="https://github.com/Chendrie98/React-Weather-App">
+        Link to my GitHub repository{" "}
+      </a>
+    </div>
+  );
+}
