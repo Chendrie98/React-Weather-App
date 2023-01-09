77ed214815ab848cef3e3954a5c43a40a58b7ae1
Chendrie98 <98337053+Chendrie98@users.noreply.github.com>
2023-01-09 17:30:43 -0500

    cleaned up some code that was unnecessary

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import GitHubLink from "./GitHubLink";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <WeatherSearch />
       <GitHubLink />
     </div>
