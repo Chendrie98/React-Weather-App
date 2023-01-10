7e39c397ef7486f678f6bb5d4fb2d804fa290fde
Chendrie98 <98337053+Chendrie98@users.noreply.github.com>
2023-01-10 16:48:04 -0500

    changed a bit of the layout

--- src/GitHubLink.js
+++ src/GitHubLink.js
@@ -3,8 +3,11 @@ import React from "react";
 export default function GitHubLink() {
   return (
     <div>
-      <a href="https://github.com/Chendrie98/React-Weather-App">
-        Link to my GitHub repository{" "}
+      <a
+        href="https://github.com/Chendrie98/React-Weather-App"
+        className="Link-Decor"
+      >
+        👉🏼 Check out my GitHub repository here!{" "}
       </a>
     </div>
   );
